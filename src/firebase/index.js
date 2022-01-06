6c7c79e34a6cae0ffd5d2c2cffb196fc68234f15
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-01-06 09:01:19 +0100

    Add firebase storage

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app as default, auth, db };
+export { app as default, auth, db, storage };
