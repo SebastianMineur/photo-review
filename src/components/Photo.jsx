047811004de6b94374bf1a26d8f2726be6a891b1
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-01-08 13:34:39 +0100

    Render skeleton image until photo has loaded

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,18 +1,39 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./Photo.module.css";
 import classes from "../util/classes";
+import imageImg from "../assets/image.png";
 
 const Photo = ({ image, onChange, className, ...props }) => {
   const [rating, setRating] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const imgRef = useRef();
 
   const handleChange = (value) => {
     setRating(value);
     onChange(value);
   };
 
+  useEffect(() => {
+    if (!imgRef.current) return;
+    imgRef.current.addEventListener("load", () => {
+      setLoading(false);
+    });
+  }, [loading]);
+
   return (
     <div className={classes(styles.Photo, className)} {...props}>
-      <img src={image.url} />
+      {loading && (
+        <img
+          src={imageImg}
+          className={classes(styles.skeleton, !loading && styles.hidden)}
+        />
+      )}
+
+      <img
+        ref={imgRef}
+        src={image.url}
+        className={classes(loading && styles.hidden)}
+      />
 
       {onChange && (
         <div className={styles.ratings}>
