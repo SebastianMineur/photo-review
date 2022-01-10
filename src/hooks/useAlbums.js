d20a6b39c2dbd337778b4d0ed86d29155d7f8848
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-01-10 15:56:10 +0100

    Sort albums by timestamp

--- src/hooks/useAlbums.js
+++ src/hooks/useAlbums.js
@@ -1,7 +1,21 @@
-import useStreamCollection from "./useStreamCollection";
+import { db } from "../firebase";
+import { collection, query, addDoc, orderBy } from "firebase/firestore";
+import useStreamQuery from "./useStreamQuery";
 
 const useAlbums = (userId) => {
-  return useStreamCollection(`users/${userId}/albums`);
+  const path = `users/${userId}/albums`;
+
+  const queryRef = query(collection(db, path), orderBy("timestamp", "desc"));
+  const streamQuery = useStreamQuery(queryRef);
+
+  const add = (data) => {
+    return addDoc(collection(db, path), data);
+  };
+
+  return {
+    ...streamQuery,
+    add,
+  };
 };
 
 export default useAlbums;
