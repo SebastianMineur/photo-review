e44ab720cff6cb74f214246fb7248f30f276646f
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-01-13 14:47:00 +0100

    Refactor file upload for readability

--- src/hooks/useFileUpload.js
+++ src/hooks/useFileUpload.js
@@ -5,6 +5,42 @@ import { v4 as uuidv4 } from "uuid";
 import { db, storage } from "../firebase";
 import { useAuthContext } from "../contexts/AuthContext";
 
+// Upload a single file and create a document for it
+const uploadFile = (file, userId, onChange = () => {}) => {
+  const uuid = uuidv4();
+  const ext = file.name.substring(file.name.lastIndexOf(".") + 1);
+  const fileRef = ref(storage, `${userId}/${uuid}.${ext}`);
+  const uploadTask = uploadBytesResumable(fileRef, file);
+
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      "state_changed",
+      // Progress handler
+      (snapshot) => onChange(snapshot.bytesTransferred / snapshot.totalBytes),
+      // Error handler
+      (error) => reject(error),
+      // Success handler
+      async () => {
+        const url = await getDownloadURL(fileRef);
+        const collectionRef = collection(db, `users/${userId}/images`);
+        // Create and return a document with metadata
+        // for this image
+        const result = await addDoc(collectionRef, {
+          name: file.name,
+          path: fileRef.fullPath,
+          size: file.size,
+          type: file.type,
+          ext,
+          url,
+          uuid,
+          albums: [],
+        });
+        resolve(result);
+      }
+    );
+  });
+};
+
 const useImageUpload = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -12,6 +48,7 @@ const useImageUpload = () => {
   const [progress, setProgress] = useState(0);
   const { currentUser } = useAuthContext();
 
+  // Upload one or more files
   const upload = async (files) => {
     setLoading(true);
     setError(null);
@@ -22,52 +59,14 @@ const useImageUpload = () => {
 
     try {
       const results = await Promise.all(
-        files.map(
-          (image, index) =>
-            new Promise((resolve, reject) => {
-              const uuid = uuidv4();
-              const ext = image.name.substring(image.name.lastIndexOf(".") + 1);
-              const fileRef = ref(storage, `${currentUser.uid}/${uuid}.${ext}`);
-              const uploadTask = uploadBytesResumable(fileRef, image);
-
-              uploadTask.on(
-                "state_changed",
-                (snapshot) => {
-                  // Calculate progress for this file
-                  progressList[index] =
-                    snapshot.bytesTransferred / snapshot.totalBytes;
-                  // Total progress for all files
-                  setProgress(
-                    progressList.reduce((acc, val) => acc + val) / files.length
-                  );
-                },
-                // Error handler
-                (error) => {
-                  reject(error);
-                },
-                // Success handler
-                async () => {
-                  const url = await getDownloadURL(fileRef);
-                  const collectionRef = collection(
-                    db,
-                    `users/${currentUser.uid}/images`
-                  );
-                  // Create and return a document with metadata
-                  // for this image
-                  const result = await addDoc(collectionRef, {
-                    name: image.name,
-                    path: fileRef.fullPath,
-                    size: image.size,
-                    type: image.type,
-                    ext,
-                    url,
-                    uuid,
-                    albums: [],
-                  });
-                  resolve(result);
-                }
-              );
-            })
+        files.map((file, index) =>
+          uploadFile(file, currentUser.uid, (progress) => {
+            progressList[index] = progress;
+            // Total progress for all files
+            setProgress(
+              progressList.reduce((acc, val) => acc + val) / files.length
+            );
+          })
         )
       );
       // All files uploaded successfully
